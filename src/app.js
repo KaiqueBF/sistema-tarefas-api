08f2db11493bc16db9331b1cd370a612f522a172
Kaique Barbosa <kaiqued64@gmail.com>
2026-06-16 16:17:29 -0300

    Adicionado health check

--- src/app.js
+++ src/app.js
@@ -1,32 +1,41 @@
 require('dotenv').config();
 require('./database/init');
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpecs = require('./docs/swagger');
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./docs/swagger');
+
 const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
 const app = express();
-const taskRoutes =
-require('./routes/taskRoutes');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
+
 app.get('/', (req, res) => {
     res.json({
         mensagem: 'API de Tarefas funcionando!'
     });
 });
 
-const PORT = process.env.PORT || 3000;
+app.get('/healthz', (req, res) => {
+    res.status(200).json({
+        status: 'ok'
+    });
+});
+
 app.use(
     '/api-docs',
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpecs)
 );
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
